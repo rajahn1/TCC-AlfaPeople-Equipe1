27eb11048218a67be19e7bd5b15671d471da13c5
rajahn01 <rafayuno@gmail.com>
2024-02-21 23:20:08 -0300

    create plugin structure

--- JavaScript/Ex4/scriptCep.js
+++ JavaScript/Ex4/scriptCep.js
@@ -1,25 +1,41 @@
 if (typeof(Logistics) === "undefined") Logistics = {};
 if (typeof(Logistics.Conta) === "undefined") Logistics.Conta = {};
-const getCEPData = require('./api.js');
+
 Logistics.Conta = {
+
+    GetCEPData: async function (CEP) {
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", `https://viacep.com.br/ws/${CEP}/json/`, false);
+        xhttp.setRequestHeader("Content-type", "application/json");
+        xhttp.send();
+        var response = JSON.parse(xhttp.responseText);
+        return response;
+    },
+
     OnChangeCEP: function(context) {
         const formContext = context.getFormContext();
+
         const CEPLabel = "address1_postalcode";
         const ruaLabel = "address1_line1";
         const cidadeLabel = "address1_city";
-        const estadoLabel = "stateorprovince";
+        const estadoLabel = "address1_stateorprovince";
 
         const CEP = formContext.getAttribute(CEPLabel).getValue();
-        console.log(CEP);
-        (async() => {
-            const data = await(getCEPData(CEP));
-            console.log(data);
-            const { cep, logradouro, bairro, localidade, uf } = data;
 
-            formContext.getAttribute(CEPLabel).setValue(cep);
-            formContext.getAttribute(ruaLabel).setValue(logradouro);
-            formContext.getAttribute(cidadeLabel).setValue(localidade);
-            formContext.getAttribute(estadoLabel).setValue(uf);
+        (async() => {
+            const data = await(Logistics.Conta.GetCEPData(CEP));
+            if (!data) {
+                formContext.getAttribute(ruaLabel).setValue("");
+                formContext.getAttribute(cidadeLabel).setValue("");
+                formContext.getAttribute(estadoLabel).setValue("");
+            }
+            else {
+                const { cep, logradouro, localidade, uf } = data;
+                formContext.getAttribute(CEPLabel).setValue(cep);
+                formContext.getAttribute(ruaLabel).setValue(logradouro);
+                formContext.getAttribute(cidadeLabel).setValue(localidade);
+                formContext.getAttribute(estadoLabel).setValue(uf);
+            }
         })();
     }
 }
