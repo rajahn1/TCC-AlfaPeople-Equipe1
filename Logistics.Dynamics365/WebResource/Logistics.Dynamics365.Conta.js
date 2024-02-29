33362723bafa0588f4192ec8c6abef0492000147
rajahn01 <rafayuno@gmail.com>
2024-02-29 11:06:54 -0300

    refactor: integrate cep script into one file

--- Logistics.Dynamics365/WebResource/Logistics.Dynamics365.Conta.js
+++ Logistics.Dynamics365/WebResource/Logistics.Dynamics365.Conta.js
@@ -12,7 +12,7 @@ Logistics.Conta = {
         }
     },
 
-    // função chamada para validar CNPJ
+    // funï¿½ï¿½o chamada para validar CNPJ
     OnChangeCNPJ : function(context) {
         const formContext = context.getFormContext();
         let meuCNPJ = formContext.getAttribute(Logistics.Conta.Enumerador.Coluna.ColunaCNPJ).getValue();
@@ -25,7 +25,7 @@ Logistics.Conta = {
             formContext.getAttribute(Logistics.Conta.Enumerador.Coluna.ColunaCNPJ).setValue(cc);
         } else { // se CNPJ nao for valido
             formContext.getAttribute(Logistics.Conta.Enumerador.Coluna.ColunaCNPJ).setValue(null);
-            Logistics.Util.Alerta("Atencao!", "CNPJ inválido!");
+            Logistics.Util.Alerta("Atencao!", "CNPJ invï¿½lido!");
         }
     },
 
@@ -40,6 +40,66 @@ Logistics.Conta = {
             let nomeFormatado = nomeConta.toLowerCase().replace(rgx, function(x) {return x.toUpperCase(); });
             formContext.getAttribute(Logistics.Conta.Enumerador.Coluna.ColunaName).setValue(nomeFormatado);
         }
-    }
+    },
 
+    fetchCEPData: async function (CEP) {
+        try {
+            var xhttp = new XMLHttpRequest();
+            xhttp.open("GET", `https://viacep.com.br/ws/${CEP}/json/`, false);
+            xhttp.setRequestHeader("Content-type", "application/json");
+            xhttp.send();
+
+            if (xhttp.status >=200 && xhttp.status < 300) {
+                var response = JSON.parse(xhttp.responseText);
+                return response;
+            } else {
+                return null;
+            }
+        } catch (error) {
+            return null;
+        }
+    },
+
+    OnChangeCEP: function(context) {
+        const formContext = context.getFormContext();
+
+        const schemaNames = {
+            CEP: "address1_postalcode",
+            estado: "address1_stateorprovince",
+            cidade: "address1_city",
+            logradouro: "log_logradouro",
+            bairro: "log_bairro",
+            complemento: "log_complemento",
+            IBGE: "log_codigoibge",
+            DDD: "log_ddd",
+        };
+
+        const CEPValue = formContext.getAttribute(schemaNames.CEP).getValue();
+
+        (async() => {
+            const CEPData = await(Logistics.Conta.fetchCEPData(CEPValue));
+
+            if (!CEPData) {
+                formContext.getAttribute(schemaNames.estado).setValue("");
+                formContext.getAttribute(schemaNames.cidade).setValue("");
+                formContext.getAttribute(schemaNames.logradouro).setValue("");
+                formContext.getAttribute(schemaNames.bairro).setValue("");
+                formContext.getAttribute(schemaNames.complemento).setValue("");
+                formContext.getAttribute(schemaNames.IBGE).setValue("");
+                formContext.getAttribute(schemaNames.DDD).setValue("");
+            }
+            else {
+                const { cep, logradouro, localidade, uf, complemento, bairro, ibge, ddd } = CEPData;
+                
+                formContext.getAttribute(schemaNames.CEP).setValue(cep);
+                formContext.getAttribute(schemaNames.estado).setValue(uf);
+                formContext.getAttribute(schemaNames.cidade).setValue(localidade);
+                formContext.getAttribute(schemaNames.logradouro).setValue(logradouro);
+                formContext.getAttribute(schemaNames.bairro).setValue(bairro);    
+                formContext.getAttribute(schemaNames.complemento).setValue(complemento);    
+                formContext.getAttribute(schemaNames.IBGE).setValue(ibge);
+                formContext.getAttribute(schemaNames.DDD).setValue(ddd);
+            }
+        })();
+    }
 }
