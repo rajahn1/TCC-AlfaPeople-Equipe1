5ea88b1394a81ce9bee7276e00b415e5f2153cbe
Celso Ito <eijuito@gmail.com>
2024-02-29 22:37:35 -0300

    feat: refactored ProductPlugin for compatibility to other plugin

--- Logistics.Dynamics365/Logistics.Dynamics365.Dynamics2.Plugin/ProductPlugin.cs
+++ Logistics.Dynamics365/Logistics.Dynamics365.Dynamics2.Plugin/ProductPlugin.cs
@@ -17,16 +17,29 @@ namespace Logistics.Dynamics365.Dynamics2.Plugin
             IOrganizationService organizationService = serviceFactory.CreateOrganizationService(context.UserId);
             ITracingService trace = (ITracingService)serviceProvider.GetService (typeof(ITracingService));
 
-            trace.Trace("Barrando criar produto");
             // Verifica se é uma operação de criação de produto
             if (context.MessageName.ToLower() == "create" && context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
             {
                 Entity product = (Entity)context.InputParameters["Target"];
 
+                var logDynamics = product.GetAttributeValue<String>("log_dynamicsid1");
+                var prodname = product.GetAttributeValue<String>("name");
+
                 // Verifica se é a entidade de produto
                 if (product.LogicalName.ToLower() == "product")
                 {
-                    throw new InvalidPluginExecutionException("Você não tem permissão para criar produtos.");
+                    trace.Trace("Barrando criar produto: " + prodname);
+
+                    // Evita bloquear cadastro vindo do plugin do ambiente Dynamics 1
+                    if (product.Contains("log_dynamicsid1"))
+                    {
+                        trace.Trace("Produto: " + prodname + " veio do dynamics 1: " + logDynamics);
+                    }
+                    else
+                    {
+                        throw new InvalidPluginExecutionException("Você não tem permissão para criar produtos.");
+                    }
+
                 }
             }
         }
